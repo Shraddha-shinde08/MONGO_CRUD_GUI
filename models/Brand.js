21902a89f8f89503caea15298f7c533b095a35f4
shraddha <shraddha.6500@gmail.com>
2025-01-24 12:24:01 +0530

    adding 3 files together

---
+++ models/Brand.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+
+//define the schema for brand
+const brandSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    description: String
+});
+
+module.exports = mongoose.model('Brand',brandSchema);
